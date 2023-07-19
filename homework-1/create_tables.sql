5f8ae79cdfd084f609d77c8538726692f62e41cd
Abramov Viacheslav <aslava@mail.ru>
2023-07-19 17:06:13 +0300

    homework-1 done

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,30 @@
 -- SQL-команды для создания таблиц
+psql
+create database north;
+
+
+CREATE TABLE customers_data
+(
+customers_id varchar(5) PRIMARY KEY,
+company_name varchar(100) NOT NULL,
+contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE employees_data
+(
+employee_id smallint PRIMARY KEY,
+first_name varchar(30) NOT NULL,
+last_name varchar(30) NOT NULL,
+title text NOT NULL,
+birth_date date NOT NULL,
+notes text
+);
+
+CREATE TABLE orders_data
+(
+order_id int PRIMARY KEY,
+customer_id varchar(5) REFERENCES customers_data(customers_id) NOT NULL,
+employee_id smallint REFERENCES employees_data(employee_id) NOT NULL,
+order_date date NOT NULL,
+ship_city varchar(30) NOT NULL
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,46 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+import pprint
+
+
+with open('.\\north_data\customers_data.csv', 'r', encoding='utf-8') as file:
+    rows = csv.DictReader(file, delimiter=',', quotechar='"')
+    customers_data = [(i['customer_id'],
+                       i['company_name'],
+                       i['contact_name']) for i in rows]
+
+with open('.\\north_data\employees_data.csv', 'r', encoding='utf-8') as file:
+    rows = csv.DictReader(file, delimiter=',', quotechar='"')
+    employees_data = [(int(i['employee_id']),
+                       i['first_name'],
+                       i['last_name'],
+                       i['title'],
+                       i['birth_date'],
+                       i['notes']) for i in rows]
+
+with open('.\\north_data\orders_data.csv', 'r', encoding='utf-8') as file:
+    rows = csv.DictReader(file, delimiter=',', quotechar='"')
+    orders_data = [(int(i['order_id']),
+                       i['customer_id'],
+                       int(i['employee_id']),
+                       i['order_date'],
+                       i['ship_city']) for i in rows]
+
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="12345"
+)
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.executemany("INSERT INTO customers_data VALUES (%s, %s, %s)", customers_data)
+            cursor.executemany("INSERT INTO employees_data VALUES (%s, %s, %s, %s, %s, %s)", employees_data)
+            cursor.executemany("INSERT INTO orders_data VALUES (%s, %s, %s, %s, %s)", orders_data)
+
+finally:
+    conn.close()
